48cabef6189c29ca6d14f5ec9157bc8c13761c02
peytonswan <98006928+peytonswan@users.noreply.github.com>
2024-10-01 21:36:23 -0700

    Update avltree.py

--- datastructures/avltree.py
+++ datastructures/avltree.py
@@ -1,1 +1,11 @@
 class AVLTree(IAVLTREE[K,V] Generic[K, V]):
+    def __init__(self):
+        self.root = None
+
+    class Node:
+        def __init__(self, key, value, height=0):
+            self.key = key 
+            self.value = value
+            self.height = height 
+            self.left = None
+            self.right = None
